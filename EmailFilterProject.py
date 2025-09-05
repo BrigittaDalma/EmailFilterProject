00074866df6d836013a8aae76f70b8c34226b1ac
BrigittaDalma <plettbrigitta@gmail.com>
2025-09-05 11:57:11 +0300

    Add files via upload
    This is a small Python project that filters simulated emails:
    - Favorite emails are moved to the top
    - Unopened newletters are skipped
    - Other emails remain in the list

---
+++ EmailFilterProject.py
@@ -0,0 +1,30 @@
+# Simulated inbox emails
+
+inbox = [
+    {"subject": "Newsletter: New season, new arrivals", "opened": False},
+    {"subject": "15% off - today only!", "opened": True},
+    {"subject": "Spam: Unlock a FREE 20$ gif card!", "opened": False},
+    {"subject": "Newsletter: Weekly science  updates", "opened": False},
+    {"subject": "Favorite store - special discount", "opened": True},
+    {"subject": "Notification: Account security update", "opened": True},
+    {"subject": "Newsletter: Tips for productive learning", "opened": False},
+]
+
+# keywords for favorite emails
+
+favorites = ["Favorite store", "Favorite game"]
+# filtering logic
+filtered_inbox = []
+
+for email in inbox:
+
+    if any (fav in email ["subject"] for fav in favorites):
+        filtered_inbox.insert(0, email)
+    elif "Newsletter" in email["subject"] and email["opened"] == False:
+        continue
+    else :
+        filtered_inbox.append(email)
+
+print("Filtered inbox:")
+for e in filtered_inbox:
+      print("-", e["subject"])
